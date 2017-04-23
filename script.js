caef44adc97c6a775361be1c33dc12ba00b85807
Daniel Domingues <dhdomingues7@gmail.com>
2017-04-23 08:40:18 -0300

    Cometarios script

--- script.js
+++ script.js
@@ -3,48 +3,48 @@ var alerta = new alert();
 
 $(document).ready(function(){
 	
-	var CSRF_TOKEN 	= $('meta[name="csrf-token"]').attr('content')
-	,	url 		= window.location.href
-	,   date 		= new Date()
-	,	mes 		= date.getMonth()
-    , 	$body 		= $('body');
+	var CSRF_TOKEN 	= $('meta[name="csrf-token"]').attr('content') // Variavel para pegar o atributo content
+	,	url 		= window.location.href // Variável para pegar o href
+	,   date 		= new Date() // Variavel pra pegar data
+	,	mes 		= date.getMonth() // Variavel pra pegar o mês
+    , 	$body 		= $('body'); // Variavel pra pegar o elemento body
 
 	addMaterialInput();
 	// buscaKeyPress() é um plugin para busca dinâmica
 	$('.busca-keypress').buscaKeyPress();
 
-	$(document).on('focus', '.material-input',addMaterialFocus);
+	$(document).on('focus', '.material-input',addMaterialFocus); // No elemento de classe .material-input quando estiver no focus chama a função AddMaterialFocus
 
-	if(localStorage.getItem('body')){
-		$body.addClass('full');
-		$('.btn-burguer').addClass('active');
+	if(localStorage.getItem('body')){ // Um if para verificar se existe o body no localstorage 
+		$body.addClass('full'); // Adiciona classe full no elemento $body
+		$('.btn-burguer').addClass('active'); // Adiciona classe active no elemento .btn-burger
 	}
 
 	$('.full [data-toggle="tooltip"]').tooltip({'placement': 'right'});
 
-	$(document).on('click', '.btn-burguer', function(){
-		let $this = $(this);
+	$(document).on('click', '.btn-burguer', function(){ // Evento de clique no elemento .btn-burguer
+		let $this = $(this); // Criação da variável $(this)
 
-	    if($('body').hasClass('full')) {
-	        $('.full [data-toggle="tooltip"]').tooltip('disable');
-	    	if(localStorage.getItem('body')){
-	    		localStorage.removeItem('body');
+	    if($('body').hasClass('full')) { // If para verificar se o body possui a classe full
+	        $('.full [data-toggle="tooltip"]').tooltip('disable'); // Desabilita o tooltip
+	    	if(localStorage.getItem('body')){ // Um if para verificar se existe o body no localstorage 
+	    		localStorage.removeItem('body'); // Se tiver o item ele remove para limpar
 	    	}
 	    } else {
-	    	localStorage.setItem('body', 'full');
+	    	localStorage.setItem('body', 'full'); // Se não ele coloca no localstorage
 	        $('[data-toggle="tooltip"]').tooltip({'placement': 'right'});
-	        $('[data-toggle="tooltip"]').tooltip("enable");
+	        $('[data-toggle="tooltip"]').tooltip("enable"); 
 	    }
 
-		$this.toggleClass('active');
-		$body.toggleClass('full');
+		$this.toggleClass('active'); // Ativa classe active
+		$body.toggleClass('full'); // Ativa classe full
 	});
 
 
-	$(document).on('click', '.notificacao', function(){
-		let $this = $(this);
+	$(document).on('click', '.notificacao', function(){ // Evento de clique no elemento .notificacao
+		let $this = $(this); // Criação da variável $(this)
 
-		$('#notificacoes').toggleClass('active');
+		$('#notificacoes').toggleClass('active'); // Ativa classe active
 	});
 
 
@@ -63,13 +63,13 @@ $(document).ready(function(){
 	////			Evento de paginacao dinâmica				////
 	////////////////////////////////////////////////////////////////
 
-	$(document).on('click', '.pagina', paginacao);
+	$(document).on('click', '.pagina', paginacao); // evento de clique no elemento .pagina chama funcao paginacao
 	////////////////////////////////////////////////////////////////
 	////				Evento de busca dinâmica				////
 	////////////////////////////////////////////////////////////////
-	$(document).on('keyup', '#busca input[type="text"]', buscaAjax);
+	$(document).on('keyup', '#busca input[type="text"]', buscaAjax); // evento de keyup no elemento #busca... chama funcao buscaAjax
 
-	$(document).on('change', '#imagem', function(event){
+	$(document).on('change', '#imagem', function(event){ // evento de troca no elemento #imagem 
 	    if (this.files && this.files[0]) {
 	        var reader = new FileReader();
 	        reader.onload = imageIsLoaded;
@@ -77,46 +77,46 @@ $(document).ready(function(){
 	    }
 	});
 
-	$(document).on('click', '.btn-add', function(event){
-		event.preventDefault();
+	$(document).on('click', '.btn-add', function(event){ // evento de clique no elemento .btn-add
+		event.preventDefault();//previne ações default
 
-		let $ingrediente = $('.ingredientes');
-		let item 		 = $ingrediente.find('.item').last().clone(true);
+		let $ingrediente = $('.ingredientes'); // criação de variável chamando elemento .ingrediente
+		let item 		 = $ingrediente.find('.item').last().clone(true); // criação variavel item
 
-		item.append('<button class="btn btn-red btn-remove-produto"><i class="fa fa-minus"></i></button>');
-		item.find('input').val('1');
-		$ingrediente.find('.item').last().after(item);
+		item.append('<button class="btn btn-red btn-remove-produto"><i class="fa fa-minus"></i></button>'); // da um append no item para adicionar o botão
+		item.find('input').val('1'); // procura o input com val 1
+		$ingrediente.find('.item').last().after(item); // procura o ultimo .item depois do item
 	});
 
-	$(document).on('click', '.btn-remove-produto', function(event){
-		let $this = $(this);
+	$(document).on('click', '.btn-remove-produto', function(event){ // Evento de clique no elemento .btn-remove-produto
+		let $this = $(this); // criação da variável $(this)
 
-		$this.parent('.item').remove();
+		$this.parent('.item').remove(); // pega o elemento pai deste item e remove
 	});
 
-	$('#loader').addClass('active');
+	$('#loader').addClass('active'); // adiciona class active no #loader
 
 
 })
 
-function imageIsLoaded(e) {
-    $('#imagem-thumb').attr('src', e.target.result);
-    $('#imagem-upload').val(e.target.result);
+function imageIsLoaded(e) { // cria funcao imageIsLoaded
+    $('#imagem-thumb').attr('src', e.target.result); // adiciona o atributo src q trás no parametro e.target.result
+    $('#imagem-upload').val(e.target.result); // adiciona o val q trás no parametro e.result
 };
 
 function addMaterialFocus(){
-	// material_input é um plugin para
-	$('input').material_input();
-	$('select').material_input();
-	$("textarea").material_input();	
+	// material_input é um plugin para inputs
+	$('input').material_input(); //cada elemetno chama a função .material_input();
+	$('select').material_input(); //cada elemetno chama a função .material_input();
+	$("textarea").material_input();	 //cada elemetno chama a função .material_input();
 }
 
 function addMaterialInput(){
 	var inputs = setInterval(function(){
-		if($(document).find("input")){
-			$('input').material_input();
-			$('select').material_input();
-			$("textarea").material_input();
+		if($(document).find("input")){ // se encontrar o elemento input
+			$('input').material_input(); //cada elemetno chama a função .material_input();
+			$('select').material_input(); //cada elemetno chama a função .material_input();
+			$("textarea").material_input(); //cada elemetno chama a função .material_input();
 			clearInterval(inputs);
 		}
 	}, 200);	
@@ -127,26 +127,26 @@ function addMaterialInput(){
 *	Função para adicionar classe do material em inputs novos
 *
 */
-function resetMaterialInput( $input ){
-	$input.each(function(key, value){
-		let $value = $(value);
-		if(!$value.siblings('label').hasClass('label-active')){
-			$value.siblings('label').addClass('label-active');
-			$value.siblings('span').addClass('input-focus');
+function resetMaterialInput( $input ){ // criação da funcao resetMaterialInput
+	$input.each(function(key, value){ // para cada input
+		let $value = $(value); // criação da variável $value chamando $(value) para melhor performance trazendo o parametro value como o elemento a ser verificado
+		if(!$value.siblings('label').hasClass('label-active')){ // se o $value irmão label tiver classe .label-active
+			$value.siblings('label').addClass('label-active'); // adiciona classe label-active ao elemento irmao label 
+			$value.siblings('span').addClass('input-focus'); // adiciona classe .input-focus no irmao span
 		}
 	})
 }
 
 function loginAjax(){
-	let data = {
-		email 	 : $('#username').val(),
-		password : $('#senha').val()
+	let data = { // criação do array data contendo email e password
+		email 	 : $('#username').val(), // criacao da variavel email pegando o val do elemento #username
+		password : $('#senha').val() // criacao da variavel password pegando o val do elemento #senha
 	};
-	if($.trim($('#username').val()) == ''){
-		alerta.danger('Insira o usuário');
+	if($.trim($('#username').val()) == ''){ // verifica se o campo de usuario está vazio, se tiver pede para inserir
+	 	alerta.danger('Insira o usuário');
 		return false;
 	}
-	if($.trim($('#senha').val()) == ''){
+	if($.trim($('#senha').val()) == ''){ // verifica se o campo de senha está vazio, se tiver pede para inserir
 		alerta.danger('Insira a senha');
 		return false;
 	}
@@ -156,46 +156,46 @@ function loginAjax(){
 		type: 'POST',
 		data: data,
 	})
-	.done(function( retorno ) {
+	.done(function( retorno ) { 
 
-		if(JSON.parse(retorno) !== 'erro'){
-			alerta.success('Logado com sucesso. Aguarde enquanto redirecionado');
-			setTimeout(() => {
+		if(JSON.parse(retorno) !== 'erro'){ // se o retorno for diferente de error
+			alerta.success('Logado com sucesso. Aguarde enquanto redirecionado'); // função alerta.sucess
+			setTimeout(() => { // adiciona um tempo de 1segundo até realizar o redirecionamento 
 				window.location = '/admin';
 			}, 1000);
-		} else{
-			alerta.danger('Login ou senha incorretos. Tente novamente!');
+		} else{ // se for = a erro  chama o alerta.danger
+			alerta.danger('Login ou senha incorretos. Tente novamente!'); 
 		}
 	})
-	.fail(function(error) {
+	.fail(function(error) { // se acontecer algo não esperado cai no erro chamando o alerta.danger
 		alerta.danger('Ocorreu um erro, contate o administrador.');
 	});
 }
 
-function buscaAjax() {	
-	let $this = $(this);
-	let busca = $this.data('busca');
+function buscaAjax() {	// Cria função buscaAjax
+	let $this = $(this); // criação da variável $this
+	let busca = $this.data('busca'); // Criação da variavel busca pegando o data-busca do elemento $this
 
 	$.get('/admin/data/busca/'+busca+'?valor='+$this.val(), function(data) {
 		// chama método com nome padrão que esta nos script de cada seção
-		ajaxAtualizaTabela( data );
+		ajaxAtualizaTabela( data ); // chama função ajaxAtualizaTabela com o parametro data
 	});
 }
 
-function paginacao() {
-	let $this 		= $(this);
-	let pagina  	= $this.text();
-	let controller 	= $this.data('controller');
+function paginacao() { // Cria função paginacao
+	let $this 		= $(this); // criação da variável $this
+	let pagina  	= $this.text(); // criação de variavel pagina pegando o elemento $this.text()
+	let controller 	= $this.data('controller'); // Criação da variavel controller pegando o data-controller do elemento $this
 
-	if($this.hasClass('active')){
+	if($this.hasClass('active')){ // verifica se tem a classe active
 		return false;
 	}
-	$('.pagina').removeClass('active');
-	$this.addClass('active');
+	$('.pagina').removeClass('active'); // remopve classe active no elemento  .pagina
+	$this.addClass('active'); // Adiciona classe active no elemento  $this
 
 	$.get('/admin/data/pagina/'+controller+'/'+ pagina, function(data) {
 		// chama método com nome padrão que esta nos script de cada seção
-		ajaxAtualizaTabela( data );
+		ajaxAtualizaTabela( data ); // chama função ajaxAtualizaTabela com o parametro data
 	});
 }
 
